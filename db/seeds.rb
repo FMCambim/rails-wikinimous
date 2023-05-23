d3768bcb67dc9cc518281563ea22e3d1c14f7de2
Francisco Cambim <cambimfrancisco@gmail.com>
2023-05-23 21:24:45 +0100

    wikinimous

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+10.times do
+  article = Article.new(title: Faker::Music::RockBand.name, content: Faker::Music::RockBand.song)
+  article.save!
+end
